0948835b80f5f724d505f76b2f5726f229df7c57
bagusadlan <bagusadlann@gmail.com>
2023-12-03 15:34:13 +0700

    feat(auth): Make register and login page

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,20 +1,21 @@
 import { Link } from 'react-router-dom'
 
-export default function LoginPage() {
+export default function RegisterPage() {
   return (
     <div className="grow flex items-center justify-around">
       <div className="mb-16">
-        <div className="text-4xl mb-4 text-center">Login</div>
+        <div className="text-4xl mb-4 text-center">Register</div>
         <form className="max-w-md mx-auto">
+          <input type="text" placeholder="John Doe" />
           <input type="email" placeholder="your@email.com" />
           <input type="password" placeholder="password" />
           <button className="bg-primary w-full py-2 px-4 rounded-2xl text-white">
-            Login
+            Register
           </button>
           <div className="text-center py-2 text-gray-500">
-            {"Don't have an account yet? "}
-            <Link to={'/register'} className="underline text-black">
-              Register now
+            {'Already a member? '}
+            <Link to={'/login'} className="underline text-black">
+              Login
             </Link>
           </div>
         </form>
