dd20b38b7482fcbad1537359ba4fab33799e326d
bagusadlan <bagusadlann@gmail.com>
2023-12-05 03:49:20 +0700

    feat(login): Implementing login with Context Provider

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,13 +1,47 @@
-import { Link } from 'react-router-dom'
+import axios from 'axios'
+import { useContext, useState } from 'react'
+import { Link, Navigate } from 'react-router-dom'
+import { UserContext } from '../UserContext'
 
 export default function LoginPage() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [redirect, setRedirect] = useState(false);
+  const {setUser} = useContext(UserContext)
+
+  async function handleLoginSubmit(e) {
+    e.preventDefault()
+    try {
+      const {data} = await axios.post('/login', {email, password})
+
+      setUser(data.data)
+      setRedirect(true)
+    } catch (error) {
+      alert('Login failed')
+    }
+  }
+
+  if (redirect) {
+    return <Navigate to={'/'} />
+  }
+
   return (
     <div className="grow flex items-center justify-around">
       <div className="mb-16">
         <div className="text-4xl mb-4 text-center">Login</div>
-        <form className="max-w-md mx-auto">
-          <input type="email" placeholder="your@email.com" />
-          <input type="password" placeholder="password" />
+        <form className="max-w-md mx-auto" onSubmit={handleLoginSubmit}>
+          <input
+            type="email"
+            placeholder="your@email.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
           <button className="bg-primary w-full py-2 px-4 rounded-2xl text-white">
             Login
           </button>
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -25,9 +25,24 @@ export default function RegisterPage() {
       <div className="mb-16">
         <div className="text-4xl mb-4 text-center">Register</div>
         <form className="max-w-md mx-auto" onSubmit={registerUser}>
-          <input type="text" placeholder="John Doe" value={name} onChange={e => setName(e.target.value)} />
-          <input type="email" placeholder="your@email.com" value={email} onChange={e => setEmail(e.target.value)} />
-          <input type="password" placeholder="password" value={password} onChange={e => setPassword(e.target.value)} />
+          <input
+            type="text"
+            placeholder="John Doe"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="email"
+            placeholder="your@email.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
           <button className="bg-primary w-full py-2 px-4 rounded-2xl text-white">
             Register
           </button>
