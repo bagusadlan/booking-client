2922049c64b109068c2dbb17c1da64ba1655a597
bagusadlan <bagusadlann@gmail.com>
2023-12-12 21:51:15 +0700

    feat(places): Handle upload photo and also upload photo by link, separate perks input to a different component

--- src/pages/PlacesPage.jsx
+++ src/pages/PlacesPage.jsx
@@ -6,11 +6,10 @@ import Perks from './Perks'
 
 function PlacesPage() {
   const { action } = useParams()
-  // const places = await axios.get('/places')
   const [title, setTitle] = useState('')
   const [address, setAddress] = useState('')
   const [addedPhotos, setAddedPhotos] = useState([])
-  const [linkPhotos, setlinkPhotos] = useState('')
+  const [photoLink, setphotoLink] = useState('')
   const [description, setDescription] = useState('')
   const [perks, setPerks] = useState([])
   const [extraInfo, setExtraInfo] = useState('')
@@ -22,6 +21,34 @@ function PlacesPage() {
     return (
       <h2 className='text-2xl mt-4'>{text}</h2>
     )
+  }
+
+  async function addPhotoByLink(e) {
+    e.preventDefault()
+
+    const {data} = await axios.post('/upload-photo-by-link', ({url: photoLink}))
+    setAddedPhotos(prev => {
+      return [...prev, data.data]
+    })
+    setphotoLink('')
+  }
+
+  async function uploadPhoto(e) {
+    const { files } = e.target
+    const data = new FormData()
+    for (let i = 0; i < files.length; i++) {
+      data.append('photos', files[i])
+    }
+    axios.post('/upload', data, {
+      headers: {
+        'Content-Type': 'multipart/formdata'
+      }
+    }).then(response => {
+      const {data} = response
+      setAddedPhotos(prev => {
+        return [...prev, ...data.data]
+      })
+    })
   }
 
   return (
@@ -43,16 +70,22 @@ function PlacesPage() {
             <input type='text' placeholder='adress' value={address} onChange={e => setAddress(e.target.value)}></input>
             {inputHeader('Photos')}
             <div className='flex gap-2'>
-              <input type='text' placeholder='add url' value={linkPhotos} onChange={e => setlinkPhotos(e.target.value)}></input>
-              <button className='bg-gray-200 px-4 rounded-2xl'>Add&nbsp;photo</button>
+              <input type='text' placeholder='add url' value={photoLink} onChange={e => setphotoLink(e.target.value)}></input>
+              <button onClick={addPhotoByLink} className='bg-gray-200 px-4 rounded-2xl'>Add&nbsp;photo</button>
             </div>
-            <div className='mt-2 mb-3 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6'>
-              <button className='border justify-center flex gap-1 border-gray-300 rounded-2xl p-8 text-gray-600'>
+            <div className='mt-2 mb-3 grid gap-2 grid-cols-3 md:grid-cols-4 lg:grid-cols-6'>
+              <label className='h-32 cursor-pointer border justify-center flex gap-1 border-gray-300 rounded-2xl text-gray-600 items-center'>
+                <input onChange={uploadPhoto} type='file' multiple className='hidden'/>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
                 </svg>
                 Upload
-              </button>
+              </label>
+              {addedPhotos.length > 0 && addedPhotos.map(link => (
+                <div className='h-32 flex'>
+                  <img src={'http://localhost:4000/uploads/'+link} key={link} className='rounded-2xl w-full object-cover'/>
+                </div>
+              ))}
             </div>
             {inputHeader('Description')}
             <textarea placeholder='description' value={description} onChange={e => setDescription(e.target.value)}/>
