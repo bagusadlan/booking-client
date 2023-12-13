44ee9549e8651f2c1180e94fcf5a11bb66a3af4b
bagusadlan <bagusadlann@gmail.com>
2023-12-13 17:05:38 +0700

    feat(places): showing all places data and edit place data

--- src/pages/PlacesPage.jsx
+++ src/pages/PlacesPage.jsx
@@ -1,8 +1,18 @@
-import { Link, useParams } from 'react-router-dom'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 
 import AccountNav from '../components/AccountNav'
 
 function PlacesPage() {
+  const [places, setPlaces] = useState([])
+  useEffect(() => {
+    axios.get('/places').then(({ data }) => {
+      console.log(data.data)
+      setPlaces(data.data)
+    })
+  }, [])
+
   return (
     <div>
       <AccountNav />
@@ -27,6 +37,26 @@ function PlacesPage() {
           </svg>
           <div>Add New Places</div>
         </Link>
+      </div>
+      <div className="mt-8 grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
+        {places.length > 0 &&
+          places.map((place) => (
+            <Link to={'/account/places/' + place._id} key={place._id} className="">
+              {place.photos.length > 0 && (
+                <div className="mb-2">
+                  <img
+                    src={'http://localhost:4000/uploads/' + place.photos[0]}
+                    className="rounded-xl w-full h-full object-cover object-center"
+                  />
+                </div>
+              )}
+              <div>
+                <h2 className="font-bold">{place.title}</h2>
+                <p className="text-gray-500">{place.description}</p>
+                <p className="text-gray-500">{place.maxGuests} Orang</p>
+              </div>
+            </Link>
+          ))}
       </div>
     </div>
   )
